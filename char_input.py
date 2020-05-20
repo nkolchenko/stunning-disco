5ae11d5c57cc1ee61c6514fb1c6176f4379f4127
nkolchenko <nkolchenko@gmail.com>
2020-05-21 00:06:58 +0200

    Created char_input.py

---
+++ char_input.py
@@ -0,0 +1,4 @@
+user_name = input("My name is Noob. What's your name? ")
+user_age = int(input("I'm one year old and you? "))
+year = str((2020 - user_age)+100)
+print(user_name + " will be 100 years old in the year " + year)
