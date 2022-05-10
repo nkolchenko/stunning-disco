8900c2c7e8f622fccb25f87214130ed76fab2583
Mykola Kolchenko <mykola.kolchenko@spryker.com>
2022-05-10 15:24:29 +1000

    this adds PEP8 formatting

--- python/string_manipulations.py
+++ python/string_manipulations.py
@@ -1,26 +1,28 @@
 # count how many times each word is used in particular string.
 
-#lipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing. Praesent eget velit tortor. Ut consectetur magna ultricies."
+#lipsum generator: https://www.lipsum.com/
+# lipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing. Praesent eget velit tortor. Ut consectetur magna ultricies."
+
 lipsum = """Amazon EKS runs a single tenant Kubernetes control plane for each cluster. The control plane infrastructure 
 isn't shared across clusters or AWS accounts. The control plane consists of at least two API server instances and three 
 etcd instances that run across three Availability Zones within an AWS Region. Amazon EKS"""
 
-my_dict={}
-incr=0
+my_dict = {}
+my_incr = 0
 
-my_lipsum=lipsum.replace(",", "").replace(".", "").lower() #getting rid of commas, dots and lowering case.
+my_lipsum = lipsum.replace(",", "").replace(".", "").lower()  # getting rid of commas, dots and lowering case.
 
 my_list = my_lipsum.split(" ")
 
-print("Words: ",len(my_list))
-while incr < len(my_list):
+print("Words Total: ", len(my_list))
+while my_incr < len(my_list):
 
     for element in my_list:
+        cnt = my_list.count(my_list[my_incr])
+        #print("word", my_incr + 1, " : ", my_list[my_incr], " found ", cnt, " times")
 
-        cnt=my_list.count(my_list[incr])
-        print("word",incr+1," : ", my_list[incr], " found ", cnt," times" )
-
-        my_dict.update({my_list[incr]:cnt})
-        incr = incr + 1
+        my_dict.update({my_list[my_incr]: cnt})
+        my_incr = my_incr + 1
 
 print(my_dict)
+my_dict.
